efbbc5a363d508a5e4b417d9ba6ba1b7ebbed6ae
Tim Makimov <TMakimov@bcsc.bc.ca>
2020-03-23 23:52:22 -0700

    Final commit

--- books-app/src/App.js
+++ books-app/src/App.js
@@ -15,33 +15,13 @@ class App extends React.Component {
     this.setState({searchItem: newItem});
   }
 
-  // componentDidMount(){
-
-  //   axios.get('https://www.googleapis.com/books/v1/volumes?q=harrypotter')
-  //   .then(response => {
-  //     console.log('Promise resolved', response.data.items);
-  //   });
-
-  // }
-
 
   render(){
-
-    //*************test passing array to multiple components. Add keys here****************
-    // let titles = ['West of Eden', 'Time Machine', 'Hollow Man'];
-    // var titleList = titles.map(function(title){
-    //     return <Book title = {title}/>;
-    // })
-    //*************test passing array to multiple components. Add keys here****************
-
     return (
       <div className="App">
         <header className="App-header">
-          
           <EntryForm addRecord = {this.addRecord} defaultValue = {this.state.searchItem}/>
           <Content searchTitle = {this.state.searchItem}/>
-        {/* <ul>{titleList}</ul> */}
-
         </header>
       </div>
     );
--- books-app/src/Content.js
+++ books-app/src/Content.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import axios from 'axios';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import PulseLoader from "react-spinners/PulseLoader";
 
 class Content extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            titles: []
+            titles: [],
+            loading: true
         };
     }
 
     fetchData =()=> {
         axios.get('https://www.googleapis.com/books/v1/volumes?q=' + this.props.searchTitle)
         .then(response => {
-            this.setState({titles: response.data.items});
-            //console.log(this.state.titles);
-        });
+            this.setState({titles: response.data.items, loading: false});
+            
+        })
     }
 
     componentDidMount(){
@@ -28,7 +30,6 @@ class Content extends React.Component{
         }
     }
 
-
     showDescription = cell => {
         return cell.description;
     }
@@ -37,31 +38,59 @@ class Content extends React.Component{
         return cell.title;
     }
 
+    showCategories = cell => {
+        if(cell.categories){
+            const data = cell.categories;
+            const categories = data.map((categoryName, index) =>
+                <li key = {index}>
+                    {categoryName}
+                </li>
+            );
+            return <ul>{categories}</ul>;
+        }
+    }
+
     showImage = cell =>{
-        return <img src={cell.imageLinks.thumbnail}/>
+        if(cell.imageLinks){
+            return <img src={cell.imageLinks.thumbnail}/>
+        }
     }
 
+    onRowClick = id => {
+        if(id.volumeInfo.infoLink){
+            window.open(id.volumeInfo.infoLink);
+        }
+    }
+    
     render(){
-        let response = this.state.titles;
-        console.log(response);
-        
-        return(
+        const response = this.state.titles;
+        const options = {
+            onRowClick: this.onRowClick
+          };
+
+          let table;
+
+          if(response){
+            table = 
             <div>
-            <BootstrapTable  ref='table' data={ response } striped hover>
-                <TableHeaderColumn dataField='id' dataSort isKey hidden>Id</TableHeaderColumn>
-                <TableHeaderColumn dataField='volumeInfo' dataFormat={this.showTitle} dataSort={ true }>Title</TableHeaderColumn>
-                <TableHeaderColumn dataField='volumeInfo' dataFormat={this.showDescription} dataSort={ true }>Description</TableHeaderColumn>
-                <TableHeaderColumn dataField='volumeInfo' dataFormat={this.showImage} dataSort={ true }>Image</TableHeaderColumn>
-            </BootstrapTable>
-        </div>
-      );
+                <BootstrapTable  ref='table' data={ response } options={options } striped hover>
+                    <TableHeaderColumn dataField='id' dataSort isKey hidden>Id</TableHeaderColumn>
+                    <TableHeaderColumn dataField='volumeInfo' dataFormat={this.showTitle} dataSort>Title</TableHeaderColumn>
+                    <TableHeaderColumn dataField='volumeInfo' dataFormat={this.showDescription} dataSort>Description</TableHeaderColumn>
+                    <TableHeaderColumn dataField='volumeInfo' dataFormat={this.showCategories} dataSort>Categories</TableHeaderColumn>
+                    <TableHeaderColumn dataField='volumeInfo' dataFormat={this.showImage} dataSort>Image</TableHeaderColumn>
+                </BootstrapTable>
+                <PulseLoader color={"blue"} loading={this.state.loading}/>
+            </div> 
+          }
+          else if(this.state.loading){
+            table = <PulseLoader color={"blue"} loading={this.state.loading}/>;
+          }
+          else{
+            table = <h1>No books found for {this.props.searchTitle}</h1>;
+          }
+        return table;
     }
-
-
 }
-
-
-  
-
 
 export default Content;
